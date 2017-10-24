ae8b7436e8e2aa0eba37d1aa197b35385b0ecc08
xiangyue <xiangyue.meng@bluegogo.com>
2017-10-24 22:54:08 +0800

    添加装饰模式

---
+++ Design_Pattern.playground/Pages/Decorator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,95 @@
+/*:
+ ### **Decorator**
+ ---
+ 
+ [回到列表](Index)
+ 
+ 1. 定义:装饰模式，动态地给一个对象添加一些额外的职责，就增加功能来说，装饰模式比生成子类更为灵活。
+ 2. 问题:咖啡店里有最基本的咖啡，还有用基本咖啡添加不同物品新调制的咖啡。
+ 3. 解决方案:定义基本咖啡，然后创建不同物品的装饰类，用装饰类装饰基本咖啡就获得了最终的咖啡了。
+ 4. 使用方法：
+     1. 定义 Coffee 协议，明确咖啡的定义。
+     2. 定义具体的 Coffee （SimpleCoffee）。
+     3. 定义咖啡的装饰类（CoffeeDecorator），在构造的时候传入要被装饰的咖啡。
+     4. 定义具体的咖啡装饰类，添加新的功能。
+ 5. 优点:
+     1. 对于扩展一个对象的功能，装饰模式比继承更加灵活性，不会导致类的个数急剧增加。
+     2. 可以通过一种动态的方式来扩展一个对象的功能，通过配置文件可以在运行时选择不同的具体装饰类，从而实现不同的行为。
+     3. 可以对一个对象进行多次装饰，通过使用不同的具体装饰类以及这些装饰类的排列组合，可以创造出很多不同行为的组合，得到功能更为强大的对象。
+     4. 具体构件类与具体装饰类可以独立变化，用户可以根据需要增加新的具体构件类和具体装饰类，原有类库代码无须改变，符合“开闭原则”。
+ 6. 缺点:
+     1. 使用装饰模式进行系统设计时将产生很多小对象，这些对象的区别在于它们之间相互连接的方式有所不同，而不是它们的类或者属性值有所不同，大量小对象的产生势必会占用更多的系统资源，在一定程序上影响程序的性能。
+     2. 装饰模式提供了一种比继承更加灵活机动的解决方案，但同时也意味着比继承更加易于出错，排错也很困难，对于多次装饰的对象，调试时寻找错误可能需要逐级排查，较为繁琐。
+ */
+
+
+import Foundation
+
+protocol Coffee {
+    func getCost() -> Double
+    func getIngredients() -> String
+}
+
+class SimpleCoffee: Coffee {
+    func getCost() -> Double {
+        return 1.0
+    }
+    
+    func getIngredients() -> String {
+        return "Coffee"
+    }
+}
+
+class CoffeeDecorator: Coffee {
+    private let decoratedCoffee: Coffee
+    fileprivate let ingredientSeparator: String = ", "
+    
+    required init(decoratedCoffee: Coffee) {
+        self.decoratedCoffee = decoratedCoffee
+    }
+    
+    func getCost() -> Double {
+        return decoratedCoffee.getCost()
+    }
+    
+    func getIngredients() -> String {
+        return decoratedCoffee.getIngredients()
+    }
+}
+
+final class Milk: CoffeeDecorator {
+    required init(decoratedCoffee: Coffee) {
+        super.init(decoratedCoffee: decoratedCoffee)
+    }
+    
+    override func getCost() -> Double {
+        return super.getCost() + 0.5
+    }
+    
+    override func getIngredients() -> String {
+        return super.getIngredients() + ingredientSeparator + "Milk"
+    }
+}
+
+final class WhipCoffee: CoffeeDecorator {
+    required init(decoratedCoffee: Coffee) {
+        super.init(decoratedCoffee: decoratedCoffee)
+    }
+    
+    override func getCost() -> Double {
+        return super.getCost() + 0.7
+    }
+    
+    override func getIngredients() -> String {
+        return super.getIngredients() + ingredientSeparator + "Whip"
+    }
+}
+/*:
+ ### Usage:
+ */
+var someCoffee: Coffee = SimpleCoffee()
+print("Cost : \(someCoffee.getCost()); Ingredients: \(someCoffee.getIngredients())")
+someCoffee = Milk(decoratedCoffee: someCoffee)
+print("Cost : \(someCoffee.getCost()); Ingredients: \(someCoffee.getIngredients())")
+someCoffee = WhipCoffee(decoratedCoffee: someCoffee)
+print("Cost : \(someCoffee.getCost()); Ingredients: \(someCoffee.getIngredients())")
