e892857117f62e529b17cea88549cdc226d5aa79
xiangyue <xiangyue.meng@bluegogo.com>
2017-12-04 21:28:27 +0800

    添加迭代器模式

---
+++ Design_Pattern.playground/Pages/Iterator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,68 @@
+/*:
+ ### **Iterator**
+ ---
+ 
+ [回到列表](Index)
+ 
+ 1. 定义:迭代器模式(Iterator Pattern)：提供一种方法来访问聚合对象，而不用暴露这个对象的内部表示，其别名为游标(Cursor)。迭代器模式是一种对象行为型模式。迭代器内部关联着需要访问的对象。
+    1. Iterator（抽象迭代器）：它定义了访问和遍历元素的接口，声明了用于遍历数据元素的方法，例如：用于获取第一个元素的first()方法，用于访问下一个元素的next()方法，用于判断是否还有下一个元素的hasNext()方法，用于获取当前元素的currentItem()方法等，在具体迭代器中将实现这些方法。
+    2. ConcreteIterator（具体迭代器）：它实现了抽象迭代器接口，完成对聚合对象的遍历，同时在具体迭代器中通过游标来记录在聚合对象中所处的当前位置，在具体实现时，游标通常是一个表示位置的非负整数。
+    3. Aggregate（抽象聚合类）：它用于存储和管理元素对象，声明一个createIterator()方法用于创建一个迭代器对象，充当抽象迭代器工厂角色。
+    4. ConcreteAggregate（具体聚合类）：它实现了在抽象聚合类中声明的createIterator()方法，该方法返回一个与该具体聚合类对应的具体迭代器ConcreteIterator实例。
+ 2. 问题:迭代输出我读过的小说，但是我们不想暴露内部的存储，这时候可以使用迭代器模式。
+ 3. 解决方案:
+ 4. 使用方法：
+    1. 定义抽象的聚合类
+    2. 定义具体的聚合类
+    3. 定义抽象迭代器，这里使用的系统的IteratorProtocol协议。
+    4. 定义具体的迭代器 NovellasIterator， 同时关联了聚合类，用于保存数据。
+ 5. 优点:
+    1. 它支持以不同的方式遍历一个聚合对象，在同一个聚合对象上可以定义多种遍历方式。在迭代器模式中只需要用一个不同的迭代器来替换原有迭代器即可改变遍历算法，我们也可以自己定义迭代器的子类以支持新的遍历方式。
+    2. 迭代器简化了聚合类。由于引入了迭代器，在原有的聚合对象中不需要再自行提供数据遍历等方法，这样可以简化聚合类的设计。
+    3. 在迭代器模式中，由于引入了抽象层，增加新的聚合类和迭代器类都很方便，无须修改原有代码，满足“开闭原则”的要求。
+ 6. 缺点:
+    1. 由于迭代器模式将存储数据和遍历数据的职责分离，增加新的聚合类需要对应增加新的迭代器类，类的个数成对增加，这在一定程度上增加了系统的复杂性。
+    2. 抽象迭代器的设计难度较大，需要充分考虑到系统将来的扩展。
+ */
+
+
+import Foundation
+
+// 中篇小说
+struct Novella {
+    let name: String
+}
+
+struct Novellas {
+    let novellas: [Novella]
+}
+
+struct NovellasIterator: IteratorProtocol {
+    
+    private var current = 0
+    private let novellas: [Novella]
+    
+    init(novellas: [Novella]) {
+        self.novellas = novellas
+    }
+    
+    mutating func next() -> Novella? {
+        defer { current += 1 }
+        return novellas.count > current ? novellas[current] : nil
+    }
+}
+
+extension Novellas: Sequence {
+    func makeIterator() -> NovellasIterator {
+        return NovellasIterator(novellas: novellas)
+    }
+}
+/*:
+ ### Usage
+ */
+let greatNovellas = Novellas(novellas: [Novella(name: "The Mist")] )
+
+for novella in greatNovellas {
+    print("I've read: \(novella)")
+}
+
