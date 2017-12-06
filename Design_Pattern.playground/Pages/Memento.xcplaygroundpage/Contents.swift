8480d7a11c5961226408b8ecc65c2e82d8bea037
xiangyue <xiangyue.meng@bluegogo.com>
2017-12-06 20:57:11 +0800

    备忘录模式

--- Design_Pattern.playground/Pages/Memento.xcplaygroundpage/Contents.swift
+++ Design_Pattern.playground/Pages/Memento.xcplaygroundpage/Contents.swift
@@ -1,7 +1,104 @@
-//: [Previous](@previous)
+/*:
+ ### **Memento**
+ ---
+ 
+ [回到列表](Index)
+ 
+ 1. 定义:备忘录模式(Memento Pattern)：在不破坏封装的前提下，捕获一个对象的内部状态，并在该对象之外保存这个状态，这样可以在以后将对象恢复到原先保存的状态。它是一种对象行为型模式，其别名为Token。
+    1. Originator（原发器）：它是一个普通类，可以创建一个备忘录，并存储它的当前内部状态，也可以使用备忘录来恢复其内部状态，一般将需要保存内部状态的类设计为原发器。
+    2. Memento（备忘录)：存储原发器的内部状态，根据原发器来决定保存哪些内部状态。备忘录的设计一般可以参考原发器的设计，根据实际需要确定备忘录类中的属性。需要注意的是，除了原发器本身与负责人类之外，备忘录对象不能直接供其他类使用，原发器的设计在不同的编程语言中实现机制会有所不同。
+    3. Caretaker（负责人）：负责人又称为管理者，它负责保存备忘录，但是不能对备忘录的内容进行操作或检查。在负责人类中可以存储一个或多个备忘录对象，它只负责存储对象，而不能修改对象，也无须知道对象的实现细节。
+ 2. 问题:在章节类的过关游戏中，每一关都会有对应的武器，我们可以在每个章节设置保存点，以后可以随时切换到保存点玩游戏。
+ 3. 解决方案: 由于需要保存游戏的状态，同时需要随时还原到某个保存点，可以考虑备忘录模式。
+ 4. 使用方法：
+    1. 定义备忘录协议，这个不是必须的，定义这个只是为了通用性
+    2. 定义原发器，这个实现备忘录协议。
+    3. 定义备忘录。例子中使用别名定义的，可以使用结构体或者其他定义，只要能够保存对应的数据就好了。
+    4. 定义负责人，负责中负责保存、恢复备忘录。例子中是按照章节保存的，恢复的时候只能根据章节恢复。如果保存的时候是按照顺序保存的，那么恢复的时候也可以按照倒序恢复，这就实现了撤销的功能。
+ 5. 优点:
+    1. 它提供了一种状态恢复的实现机制，使得用户可以方便地回到一个特定的历史步骤，当新的状态无效或者存在问题时，可以使用暂时存储起来的备忘录将状态复原。
+    2. 备忘录实现了对信息的封装，一个备忘录对象是一种原发器对象状态的表示，不会被其他代码所改动。备忘录保存了原发器的状态，采用列表、堆栈等集合来存储备忘录对象可以实现多次撤销操作。
+ 6. 缺点:
+    1. 资源消耗过大，如果需要保存的原发器类的成员变量太多，就不可避免需要占用大量的存储空间，每保存一次对象的状态都需要消耗一定的系统资源。
+ */
 
 import Foundation
 
-var str = "Hello, playground"
+/*:
+ Memento
+ */
+typealias Memento = NSDictionary
+/*:
+ Originator
+ */
+protocol MementoConvertible {
+    var memento: Memento { get }
+    init?(memento: Memento)
+}
 
-//: [Next](@next)
+struct GameState: MementoConvertible {
+    
+    private struct Keys {
+        static let chapter = "com.valve.halflife.chapter"
+        static let weapon = "com.valve.halflife.weapon"
+    }
+    
+    var chapter: String
+    var weapon: String
+    
+    init(chapter: String, weapon: String) {
+        self.chapter = chapter
+        self.weapon = weapon
+    }
+    
+    init?(memento: Memento) {
+        guard let mementoChapter = memento[Keys.chapter] as? String,
+            let mementoWeapon = memento[Keys.weapon] as? String else {
+                return nil
+        }
+        
+        chapter = mementoChapter
+        weapon = mementoWeapon
+    }
+    
+    var memento: Memento {
+        return [ Keys.chapter: chapter, Keys.weapon: weapon ]
+    }
+}
+/*:
+ Caretaker
+ */
+enum CheckPoint {
+    static func save(_ state: MementoConvertible, saveName: String) {
+        let defaults = UserDefaults.standard
+        defaults.set(state.memento, forKey: saveName)
+        defaults.synchronize()
+    }
+    
+    static func restore(saveName: String) -> Memento? {
+        let defaults = UserDefaults.standard
+        
+        return defaults.object(forKey: saveName) as? Memento
+    }
+}
+/*:
+ ### Usage
+ */
+var gameState = GameState(chapter: "Black Mesa Inbound", weapon: "Crowbar")
+
+gameState.chapter = "Anomalous Materials"
+gameState.weapon = "Glock 17"
+CheckPoint.save(gameState, saveName: "gameState1")
+
+gameState.chapter = "Unforeseen Consequences"
+gameState.weapon = "MP5"
+CheckPoint.save(gameState, saveName: "gameState2")
+
+gameState.chapter = "Office Complex"
+gameState.weapon = "Crossbow"
+CheckPoint.save(gameState, saveName: "gameState3")
+
+if let memento = CheckPoint.restore(saveName: "gameState1") {
+    let finalState = GameState(memento: memento)
+    dump(finalState)
+}
